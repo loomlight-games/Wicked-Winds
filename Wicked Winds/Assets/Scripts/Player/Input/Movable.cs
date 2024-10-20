fecaaffaa054d6ebad30902318264602239a2e4b
fkavaro <by.fka.varo@gmail.com>
2024-10-20 20:43:22 +0200

    flying joystick bug persists

--- Wicked Winds/Assets/Scripts/Player/Input/Movable.cs
+++ Wicked Winds/Assets/Scripts/Player/Input/Movable.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 /// Detects inputs (joystick and keyboard) and moves object
 /// Only runs when enough boost
 /// </summary>
+[RequireComponent(typeof(CharacterController))]
 public class Movable : MonoBehaviour
 {
 // Properties ////////////////////////////////////////////////////
@@ -19,7 +20,7 @@ public class Movable : MonoBehaviour
     Vector2 movement2D; // Joystick
     Vector3 movement3D, lookAtPosition; // Player
     Quaternion lookAtRotation;
-    float verticalVelocity;
+    float verticalVelocity, movementSpeed;
     float joystickScale = 2f;
     bool canRun;
     // Inspector
@@ -69,7 +70,110 @@ public class Movable : MonoBehaviour
 
     ///////////////////////////////////////////////////////////////////////
     /// <summary>
-    /// Detects Joystickinputs
+    /// Gravity calculation and flying behavior
+    /// </summary>
+    void Fly()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1f; // Slight downward force to keep grounded
+            
+            if (jumpKey) // If jump is pressed while grounded
+                verticalVelocity = jumpForce; // Apply jump force
+        }
+        else
+        {
+            if (jumpKey) // If jump is pressed while in the air, ascend
+                verticalVelocity += jumpForce * Time.deltaTime; // Gradually increase altitude
+            else // Apply gravity when jump is not pressed
+                verticalVelocity -= gravity * Time.deltaTime; // Descend naturally
+        }
+    }
+
+    /// <summary>
+    /// Moves player in isometric perspective.
+    /// </summary>
+    void Move(){
+        // Limit joystick movement and check if it's running
+        CheckJoystick();
+
+        // Get direction in 3D space based on camera orientation
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        // Ensure the camera's movement stays on the horizontal plane
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize(); // Magnitude of 1
+        right.Normalize(); // Magnitude of 1
+
+        // 3DMovement based on 2D input and camera's orientation, without veretical volicity applied
+        movement3D = right * movement2D.x + forward * movement2D.y;
+
+        // Check if the player is running (on the ground or in the air)
+        if (runKey || runJoystick)
+        {
+            if (canRun)  // If able to run (boost available)
+            {
+                if (movement2D.sqrMagnitude != 0) // Is moving
+                    RunningEvent?.Invoke(this, null); // Trigger running event
+                
+                movementSpeed = runSpeed;
+            }
+            else  // If not able to run, walk
+                movementSpeed = walkSpeed;
+        }
+        else
+            movementSpeed = walkSpeed;
+
+        // Move character applying vertical velocity
+        controller.Move((movement3D * movementSpeed + verticalVelocity * Vector3.up) * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Rotates player to face movement and maintains it after stopping
+    /// </summary>
+    void Rotate(){
+        // Only rotate if there is movement input
+        // To ensure that rotation moving is maintained when stopping
+        if (movement3D.x != 0 || movement3D.z != 0)
+        {
+            lookAtPosition = new Vector3(movement3D.x, 0, movement3D.z); // Movement direction
+            lookAtRotation = Quaternion.LookRotation(lookAtPosition); // Rotation to movement direction
+            // From current rotation to movement rotation
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookAtRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+        /// <summary>
+    /// Check if joystick has passed keyboard limits to run
+    /// </summary>
+    private void CheckJoystick()
+    {
+        // Keyboard inputs are normalized (-1>0<1)
+        if (movement2D.x > 1 || movement2D.y > 1 || 
+            movement2D.x < -1 || movement2D.y < -1){
+            // Limit movement passed keyboard values
+            // Ensures the movemeent difference between both inputs is minimal
+            if (movement2D.x > 1)
+                movement2D.x = 1.1f;
+            else if (movement2D.x < -1)
+                movement2D.x = -1.1f;
+            if (movement2D.y > 1)
+                movement2D.y = 1.1f;
+            else if (movement2D.y < -1)
+                movement2D.y = -1.1f;
+
+            runJoystick = true;
+        }
+        else{
+            runJoystick = false;
+        }
+    }
+
+    /// <summary>
+    /// Detects joystick inputs
     /// </summary>
     public void OnStick(InputAction.CallbackContext context)
     {
@@ -111,119 +215,5 @@ public class Movable : MonoBehaviour
             canRun = false; // Can't run
         else
             canRun = true;
-    }
-
-    /// <summary>
-    /// Check if joystick has passed keyboard limits to run
-    /// </summary>
-    private void CheckJoystick()
-    {
-        // Keyboard inputs are normalized (-1>0<1)
-        if (movement2D.x > 1 || movement2D.y > 1 || 
-            movement2D.x < -1 || movement2D.y < -1){
-            // Limit movement passed keyboard values
-            // Ensures the movemeent difference between both inputs is minimal
-            if (movement2D.x > 1)
-                movement2D.x = 1.1f;
-            else if (movement2D.x < -1)
-                movement2D.x = -1.1f;
-            if (movement2D.y > 1)
-                movement2D.y = 1.1f;
-            else if (movement2D.y < -1)
-                movement2D.y = -1.1f;
-
-            runJoystick = true;
-        }
-        else{
-            runJoystick = false;
-        }
-    }
-    
-    /// <summary>
-    /// Gravity calculation and flying behavior
-    /// </summary>
-    void Fly()
-    {
-        if (controller.isGrounded)
-        {
-            verticalVelocity = -1f; // Slight downward force to keep grounded
-            
-            if (jumpKey) // If jump is pressed while grounded
-            {
-                verticalVelocity = jumpForce; // Apply jump force
-            }
-        }
-        else
-        {
-            if (jumpKey) // If jump is pressed while in the air, ascend
-            {
-                verticalVelocity += jumpForce * Time.deltaTime; // Gradually increase altitude
-            }
-            else // Apply gravity when jump is not pressed
-            {
-                verticalVelocity -= gravity * Time.deltaTime; // Descend naturally
-            }
-        }
-    }
-
-    /// <summary>
-    /// Moves player in isometric perspective.
-    /// </summary>
-    void Move(){
-        // Limit joystick movement and check if it's running
-        CheckJoystick();
-
-        // Get direction in 3D space based on camera orientation
-        Vector3 forward = cameraTransform.forward;
-        Vector3 right = cameraTransform.right;
-
-        // Ensure the camera's movement stays on the horizontal plane
-        forward.y = 0f;
-        right.y = 0f;
-
-        forward.Normalize(); // Magnitude of 1
-        right.Normalize(); // Magnitude of 1
-
-        // 3DMovement based on 2D input and camera's orientation
-        movement3D = right * movement2D.x + forward * movement2D.y;
-        
-        // Ensure vertical speed (gravity or flight) does not affect horizontal movement
-        Vector3 horizontalMovement = new(movement3D.x, 0, movement3D.z);
-
-        // Apply the vertical velocity calculated in Fly() to movement3D
-        movement3D.y = verticalVelocity;
-
-        // Check if the player is running (on the ground or in the air)
-        if (runKey || runJoystick)
-        {
-            if (canRun)  // If able to run (boost available)
-            {
-                RunningEvent?.Invoke(this, null); // Trigger running event
-                controller.Move(runSpeed * Time.deltaTime * horizontalMovement + movement3D.y * Time.deltaTime * Vector3.up); // Run and fly
-            }
-            else  // If not able to run, walk
-            {
-                controller.Move(walkSpeed * Time.deltaTime * horizontalMovement + movement3D.y * Time.deltaTime * Vector3.up); // Walk and fly
-            }
-        }
-        else
-        {
-            controller.Move(walkSpeed * Time.deltaTime * horizontalMovement + movement3D.y * Time.deltaTime * Vector3.up); // Walk and fly
-        } 
-    }
-
-    /// <summary>
-    /// Rotates player to face movement and maintains it after stopping
-    /// </summary>
-    void Rotate(){
-        // Only rotate if there is movement input
-        // To ensure that rotation moving is maintained when stopping
-        if (movement3D.x != 0 || movement3D.z != 0)
-        {
-            lookAtPosition = new Vector3(movement3D.x, 0, movement3D.z); // Movement direction
-            lookAtRotation = Quaternion.LookRotation(lookAtPosition); // Rotation to movement direction
-            // From current rotation to movement rotation
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookAtRotation, rotationSpeed * Time.deltaTime);
-        }
     }
 }
