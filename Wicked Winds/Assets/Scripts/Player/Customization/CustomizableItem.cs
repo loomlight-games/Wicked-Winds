a8ea19caaf926e06d248b820e61becce635b1c69
fkavaro <by.fka.varo@gmail.com>
2024-10-24 04:33:09 +0200

    Shop UI remastered

--- Wicked Winds/Assets/Scripts/Player/Customization/CustomizableItem.cs
+++ Wicked Winds/Assets/Scripts/Player/Customization/CustomizableItem.cs
@@ -7,4 +7,5 @@ public class CustomizableItem : MonoBehaviour
     public CustomizableCharacter.BodyPart bodyPart;
     public GameObject prefab;  // Reference to the prefab
     public GameObject instance;  // Instance of the item in the scene
+    public int price; // Coins this item costs
 }
