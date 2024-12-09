833f5737a6aee30178e467c6690ccf3aa5ab3041
fkavaro <by.fka.varo@gmail.com>
2024-12-09 19:19:07 +0100

    Cat animations fixed

--- Wicked Winds/Assets/Scripts/Animations/Creatures/CatAnimator.cs
+++ Wicked Winds/Assets/Scripts/Animations/Creatures/CatAnimator.cs
@@ -8,11 +8,12 @@ public class CatAnimator : MonoBehaviour
     CatController catController;
 
     readonly int Idle = Animator.StringToHash("Idle"),
-        Moving = Animator.StringToHash("Moving");
+                Moving = Animator.StringToHash("Moving");
 
     int currentAnimation;
 
-    void Start(){
+    void Start()
+    {
         catController = GetComponent<CatController>();
     }
 
@@ -25,20 +26,23 @@ public class CatAnimator : MonoBehaviour
     /// <summary>
     /// Checks animation conditions
     /// </summary>
-    void CheckAnimation(){
+    void CheckAnimation()
+    {
         // Is still
         if (catController.currentStateName == "IdleState")
             ChangeAnimation(Idle);
-        else 
+        else
             ChangeAnimation(Moving);
     }
 
     /// <summary>
     /// Transitions to given animation 
     /// </summary>
-    void ChangeAnimation(int newAnimation, float duration = 0.2f){
+    void ChangeAnimation(int newAnimation, float duration = 0.2f)
+    {
         // Not same as current
-        if (currentAnimation != newAnimation){
+        if (currentAnimation != newAnimation)
+        {
             currentAnimation = newAnimation;
             // Interpolate transition to new animation
             catController.animator.CrossFade(newAnimation, duration);
