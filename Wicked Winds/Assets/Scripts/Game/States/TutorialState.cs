a9188f2f4fa87f704f4b8ada3908519cd98e7cd7
albxharo <albahb2003@gmail.com>
2024-12-12 21:15:01 +0100

    Almost finsihed Ui

--- Wicked Winds/Assets/Scripts/Game/States/TutorialState.cs
+++ Wicked Winds/Assets/Scripts/Game/States/TutorialState.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class TutorialState : AState
 {
-    GameObject Menu, panel;
+    GameObject menu, panel, howToPlay, contenedorPC, contenedorMovil, contenedor;
+
     public override void Enter()
     {
-        Menu = GameObject.Find("TutorialPanel");
-        panel = Menu.transform.Find("Panel").gameObject;
+        // Buscar los elementos en la jerarquía
+        menu = GameObject.Find("TutorialPanel");
+        panel = menu.transform.Find("Panel").gameObject;
+        howToPlay = panel.transform.Find("ScrollView/LevelPages/HowToPlayPage").gameObject;
+
+        // Encontrar los contenedores específicos
+        contenedor = howToPlay.transform.Find("Contenedor").gameObject;
+        contenedorPC = contenedor.transform.Find("ContenedorPC").gameObject;
+        contenedorMovil = contenedor.transform.Find("ContenedorMovil").gameObject;
+
+        // Activar el panel principal
         panel.SetActive(true);
 
+        // Mostrar el contenedor adecuado según la variable playingOnPC
+        if (GameManager.Instance.playingOnPC)
+        {
+            contenedorPC.SetActive(true);
+            contenedorMovil.SetActive(false);
+        }
+        else
+        {
+            contenedorPC.SetActive(false);
+            contenedorMovil.SetActive(true);
+        }
     }
+
     public override void Update()
     {
         base.Update();
     }
-    public override void Exit() 
+
+    public override void Exit()
     {
+        // Ocultar el panel completo al salir del estado
         panel.SetActive(false);
-    }    
+    }
 }
